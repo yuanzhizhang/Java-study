4d1c4fcb1085c804483b7cc91c8a5c8109abda31
Yuanzhi Zhang <1360268873@qq.com>
2020-05-22 21:35:36 +0800

    方法

---
+++ 程序设计基础/5.方法/code/Repeater.java
@@ -0,0 +1,10 @@
+public class Repeater{			//repeater 复读机
+    public static void Repeat(){
+        for (int i=0; i<10; i++){
+            System.out.println("我是复读机");
+        }
+    }
+    public static void main(String[] args){
+        Repeat();
+    }
+}
